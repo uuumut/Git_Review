1f098e0968afffb46df23e846251cb09dff9d9bc
Umut <umut19950123@gmail.com>
2022-08-23 08:55:55 -0700

    world is done

--- src/endOfWorld/theWorld.java
+++ src/endOfWorld/theWorld.java
@@ -4,5 +4,6 @@ public class theWorld {
     public static void main(String[] args) {
         System.out.println("world about to end");
         System.out.println("bye world");
+        System.out.println("world is ended");
     }
 }
