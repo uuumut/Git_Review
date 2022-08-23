46e8c969e99a090925b45f9b90b6799b43a44405
Umut <umut19950123@gmail.com>
2022-08-23 08:55:21 -0700

    say bye to world

--- src/endOfWorld/theWorld.java
+++ src/endOfWorld/theWorld.java
@@ -3,5 +3,6 @@ package endOfWorld;
 public class theWorld {
     public static void main(String[] args) {
         System.out.println("world about to end");
+        System.out.println("bye world");
     }
 }
